f97f24012287743d8ef421073cde12f65fdf762c
Yevan-0 <yevand2006@gmail.com>
2026-04-07 15:47:52 +0530

    feat: added meta tags to home  page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata ={
+  title:"Home",
+  description:"This is the home page"
+}
 
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center" >
       <div className="space-y-3 max-w-3xl"><h1 className="text-5xl font-semibold">Home Page</h1>
 
-        <p className="text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur s
-          int occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <p className="text-gray-400">This is the home page</p>
       </div>
 
     </div>
